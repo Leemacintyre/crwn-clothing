2e13ee2cbbb727ea92a0ee5f196a30e60a0d67b3
Lee Macintyre <macintyrelee@gmail.com>
2020-12-27 08:24:44 +0000

    updating stripe intergrationand updating our title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -36,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => (
 		<div className='total'>
 			<span>TOTAL ${total}</span>
 		</div>
+		<div className='test-warning'>
+			*Please use the following test credit card for payment*
+			<br />
+			4242 4242 4242 4242 - exp: 01/23 - cvv:123
+		</div>
+		<StripeCheckoutButton price={total} />
 	</div>
 );
 
